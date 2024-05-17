c9f0f16daf7584b123b3f97691e3d615003ecbb7
dzhyranov <v.dzhyranov@gmail.com>
2024-05-17 22:22:03 +0300

    solution

--- task.sql
+++ task.sql
@@ -9,25 +9,41 @@ CREATE TABLE Countries (
     PRIMARY KEY (ID)
 );
 
+CRETE TABLE Warehouse (
+    ID INT,
+    Name VARCHAR(50),
+    Address VARCHAR(50),
+    CountryID INT,
+    PRIMARY KEY (ID),
+	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION
+);
+
+CREATE TABLE Products (
+    ProductID INT,
+    ProductName VARCHAR(50),
+    PRIMARY KEY (ProductID)
+);
+
 CREATE TABLE ProductInventory (
     ID INT,
-    ProductName VARCHAR(50),
+    ProductID INT,
+    WarehouseID INT,
     WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
-    CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+	FOREIGN KEY (ProductID) REFERENCES PRODUCTS(ID) ON DELETE NO ACTION,
+	FOREIGN KEY (WarehouseID) REFERENCES Warehouse(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
 
 -- Populate test data
 
 INSERT INTO Countries (ID,Name)
-	VALUES (1, 'Country1');
-INSERT INTO Countries (ID,Name)
-	VALUES (2, 'Country2');
-    
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+	VALUES (2, 'Country2'), (1, 'Country1');
+
+INSERT INTO Warehouses (ID, WarehouseName, WarehouseAddress, CountryID)
+    VALUES (1, 'Warehouse-1', 'City-1, Street-1', 1), (2, 'Warehouse-2', 'City-2, Street-2', 2);
+
+INSERT INTO PRODUCTS (ID,ProductName)
+	VALUES (1, 'AwesomeProduct'), (2, 'AwesomeProduct');
+
+INSERT INTO ProductInventory (ID,ProductID,WarehouseID,WarehouseAmount)
+	VALUES (1, 1, 1, 2), (2, 2, 2, 5);
